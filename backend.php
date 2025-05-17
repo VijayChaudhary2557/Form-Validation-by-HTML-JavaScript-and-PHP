c1756b8e174ff4021ae18a41cd1b17b3d71c1801
VijayChaudhary2557 <155866301+VijayChaudhary2557@users.noreply.github.com>
2025-05-17 21:34:25 +0530

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,212 +1,129 @@
 <?php
-    session_start();
-    $conn = mysqli_connect("sql12.freesqldatabase.com","sql12779399","kA4qNbKN3B","sql12779399");
+session_start();
+$conn = mysqli_connect("sql12.freesqldatabase.com","sql12779399","kA4qNbKN3B","sql12779399");
 
-    if (isset($_POST["submit"])) {
-        $_SESSION["name"] = $_POST["name"];
-        $_SESSION["email"] = $_POST["email"];
-        $_SESSION["password"] = $_POST["password"];
-        $_SESSION["cpassword"] = $_POST["cpassword"];
-        $_SESSION["gender"] = $_POST["gender"];
+if (isset($_POST["submit"])) {
+    $_SESSION["name"] = $_POST["name"];
+    $_SESSION["email"] = $_POST["email"];
+    $_SESSION["password"] = $_POST["password"];
+    $_SESSION["cpassword"] = $_POST["cpassword"];
+    $_SESSION["gender"] = $_POST["gender"];
 
-        if (isset($_POST["hobbie"])) {
-            $_SESSION["hobbies"] = $_POST["hobbie"];
-            $hobbieLen = count($_SESSION["hobbies"]);
+    if (isset($_POST["hobbie"])) {
+        $_SESSION["hobbies"] = $_POST["hobbie"];
+        $hobbieLen = count($_SESSION["hobbies"]);
+    } else {
+        $_SESSION["hobbies"] = array(); 
+        $hobbieLen = 0;
+    }
+    $hobbies = json_encode($_SESSION["hobbies"]);
+
+    $_SESSION["city"] = $_POST["city"];
+
+    $_SESSION["pname"] = $_FILES["photo"]["name"];
+    $_SESSION["tloc"] = $_FILES['photo']['tmp_name'];
+    $_SESSION["size"] = $_FILES['photo']['size'];
+    $_SESSION["type"] = $_FILES['photo']['type'];
+
+    $nameRegex = "/^[a-zA-Z]+(?:\s[a-zA-Z]+)*$/";
+    $emailRegex = "/^([\w]*[\w\.]*(?!\.)@gmail.com)/";
+    $passRegex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/";
+    $ext = array('image/jpeg', 'image/jpg');
+
+    $typeErr = false;
+    $sizeErr = false;
+
+    // Validation
+    $_SESSION["nameErr"] = ($_SESSION["name"] == "") ? "Name should be required." :
+                            (!preg_match($nameRegex, $_SESSION["name"]) ? "Enter valid Name." : "");
+
+    $_SESSION["emailErr"] = ($_SESSION["email"] == "") ? "Email should be required." :
+                             (!preg_match($emailRegex, $_SESSION["email"]) ? "Enter valid Email." : "");
+
+    $_SESSION["passErr"] = ($_SESSION["password"] == "") ? "Password should be required." :
+                             (!preg_match($passRegex, $_SESSION["password"]) ? "Enter valid Password." : "");
+
+    $_SESSION["cpassErr"] = ($_SESSION["cpassword"] == "") ? "Conform Password should be required." :
+                              ($_SESSION["password"] != $_SESSION["cpassword"] ? "Password not matched." : "");
+
+    $_SESSION["genderErr"] = ($_SESSION["gender"] == "") ? "Gender should be required." : "";
+
+    $_SESSION["hobbiesErr"] = ($hobbieLen == 0) ? "Hobbie should be required." : "";
+
+    $_SESSION["cityErr"] = ($_SESSION["city"] == "") ? "City should be required." : "";
+
+    if ($_SESSION["pname"] == "") {
+        $_SESSION["photoErr"] = "Photo should be required.";
+    } elseif (!in_array($_SESSION["type"], $ext)) {
+        $_SESSION["photoErr"] = "Image should be jpeg, jpg";
+        $typeErr = true;
+    } elseif ($_SESSION["size"] > 1000000) {
+        $_SESSION["photoErr"] = "Image size should be less than 1 mb";
+        $sizeErr = true;
+    } else {
+        $_SESSION["photoErr"] = "";
+    }
+
+    // Final validation check
+    if (
+        $_SESSION["nameErr"] == "" &&
+        $_SESSION["emailErr"] == "" &&
+        $_SESSION["passErr"] == "" &&
+        $_SESSION["cpassErr"] == "" &&
+        $_SESSION["genderErr"] == "" &&
+        $_SESSION["hobbiesErr"] == "" &&
+        $_SESSION["cityErr"] == "" &&
+        $_SESSION["photoErr"] == "" &&
+        $typeErr == false &&
+        $sizeErr == false
+    ) {
+        // All validations passed
+        $name = $_SESSION["name"];
+        $email = $_SESSION["email"];
+        $password = $_SESSION["password"];
+        $cpassword = $_SESSION["cpassword"];
+        $gender = $_SESSION["gender"];
+        $city = $_SESSION["city"];
+        $pname = $_SESSION["pname"];
+        $tloc = $_SESSION["tloc"];
+
+        // Handle duplicate filenames
+        $i = 1;
+        if (file_exists("Images/" . $pname)) {
+            $temp = explode(".", $pname);
+            $end = "." . end($temp);
+            $nam = rtrim($pname, $end);
+            $tempName = $nam . $i . $end;
+            while (file_exists("Images/" . $tempName)) {
+                $i++;
+                $tempName = $nam . $i . $end;
+            }
+            $pname = $tempName;
+        }
+
+        // Insert query
+        $query = "INSERT INTO validform VALUES (DEFAULT, '$name', '$email', '$password', '$cpassword', '$gender', '$hobbies', '$city', '$pname')";
+        if (mysqli_query($conn, $query)) {
+            if (move_uploaded_file($tloc, "Images/" . $pname)) {
+                $_SESSION['result'] = "Form Successfully Submitted";
+                $_SESSION["showing"] = $pname;
+
+                header("Location: preview.php");
+                exit(); // Stop execution
+            } else {
+                $_SESSION["uploadErr"] = "Failed to upload file.";
+                header("Location: index.php");
+                exit();
+            }
         } else {
-            $_SESSION["hobbies"] = array(); // Initialize an empty array
-            $hobbieLen = 0;
-        }
-        $hobbies = json_encode($_SESSION["hobbies"]);
-
-        $_SESSION["city"] = $_POST["city"];
-
-        $_SESSION["pname"] = $_FILES["photo"]["name"];
-        $_SESSION["tloc"] = $_FILES['photo']['tmp_name'];
-	    $_SESSION["size"] = $_FILES['photo']['size'];
-	    $_SESSION["type"] = $_FILES['photo']['type'];
-
-
-        $nameRegex = "/^[a-zA-Z]+(?:\s[a-zA-Z]+)*$/";
-        $emailRegex = "/^([\w]*[\w\.]*(?!\.)@gmail.com)/";
-        $passRegex = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/";
-
-        $ext=array('image/jpeg', 'image/jpg');  
-        
-        $typeErr = false;
-        $sizeErr = false;
-
-        if($_SESSION["name"]=="")
-        {
-            $_SESSION["nameErr"] = "Name should be required.";
-        }
-        else if(preg_match($nameRegex, $_SESSION["name"]))
-        {
-            $_SESSION["nameErr"] = "";
-        }
-        else
-        {
-            $_SESSION["nameErr"] = "Enter valid Name.";
-        }
-
-        if($_SESSION["email"]=="")
-        {
-            $_SESSION["emailErr"] = "Email should be required.";
-        }
-        else if(preg_match($emailRegex, $_SESSION["email"]))
-        {
-            $_SESSION["emailErr"] = "";
-        }
-        else
-        {
-            $_SESSION["emailErr"] = "Enter valid Email.";
-        }
-
-
-        if($_SESSION["password"]=="")
-        {
-            $_SESSION["passErr"] = "Password should be required." ;
-        }
-        else if(preg_match($passRegex, $_SESSION["password"]))
-        {
-            $_SESSION["passErr"] = "";
-        }
-        else
-        {
-            $_SESSION["passErr"] = "Enter valid Password.";
-
-        }
-
-
-        if($_SESSION["cpassword"]=="")
-        {
-            $_SESSION["cpassErr"] = "Conform Password should be required." ;
-        }
-        else if($_SESSION["password"]==$_SESSION["cpassword"])
-        {
-            $_SESSION["cpassErr"] = "" ;
-        }
-        else
-        {
-            $_SESSION["cpassErr"] = "Password not matched." ;
-        }
-
-        if($_SESSION["gender"]=="")
-        {
-            $_SESSION["genderErr"] = "Gender should be required." ;
-        }
-        else
-        {
-            $_SESSION["genderErr"] = "" ;
-        }
-
-        if($hobbieLen==0)
-        {
-            $_SESSION["hobbiesErr"] = "Hobbie should be required." ;
-        }
-        else
-        {
-            $_SESSION["hobbiesErr"] = "" ;
-        }
-
-        if($_SESSION["city"]=="")
-        {
-            $_SESSION["cityErr"] = "City should be required." ;
-        }
-        else
-        {
-            $_SESSION["cityErr"] = "" ;
-        }
-
-        if($_SESSION["pname"]=="")
-        {
-            $_SESSION["photoErr"] = "Photo should be required." ;
-        }
-        else if(!in_array($_SESSION['type'],$ext))
-        {
-            $_SESSION['photoErr'] = "Image should be jpeg, jpg";
-            $typeErr = true;
-        }
-        else if($_SESSION["size"] > 1000000)
-        {
-            $_SESSION['photoErr'] = "Image size should be less than 1 mb";
-            $sizeErr = true;
-        }
-        else
-        {
-            $_SESSION["photoErr"] = "" ;
-            $typeErr = false;
-            $sizeErr = false;
-        }
-
-        echo "Hobbie:".$_SESSION["hobbiesErr"]."<br>";
-        echo "Pname:".$_SESSION["photoErr"]."<br>";
-        echo "tloc:".$_SESSION["tloc"]."<br>";
-        echo "size:".$_SESSION["size"]."<br>";
-        echo "type:".$_SESSION["type"]."<br>";
-
-        if(($_SESSION["name"]!= "" && $_SESSION["email"]!= "" && $_SESSION["password"]!= "" && $_SESSION["cpassword"]!= "" && $_SESSION["gender"]!= "" && $_SESSION["pname"]!= "" && $_SESSION["city"]!= "") && count($_SESSION["hobbies"])!=0 && ($typeErr==false && $sizeErr==false))
-        {
-            if($_SESSION["nameErr"]=="" && $_SESSION["emailErr"]=="" && $_SESSION["passErr"]=="" && $_SESSION["cpassErr"] == "" && $_SESSION["genderErr"] == "" &&  $_SESSION["hobbiesErr"] == "" && $_SESSION["cityErr"] == "" && $_SESSION["photoErr"]=="")
-            {
-                $name = $_SESSION["name"];
-                $email = $_SESSION["email"];
-                $password = $_SESSION["password"];
-                $cpassword = $_SESSION["cpassword"];
-
-                $gender = $_SESSION["gender"];
-                $city = $_SESSION["city"];
-                $pname = $_SESSION["pname"];
-                $tloc = $_SESSION["tloc"];
-                
-                $i=1;
-
-                if(file_exists("Images/".$pname))
-                {
-                    $temp = explode(".", $pname);
-                    $end = ".".end($temp);
-                    $nam = rtrim($pname, $end);
-                    $tempName = $nam.$i.$end;
-                    while(file_exists("Images/".$tempName))
-                    {
-                        $i++;
-                        $tempName = $nam.$i.$end;
-                    }
-                    $pname = $tempName;
-                }
-
-                $query = "Insert into validform values (DEFAULT, '$name', '$email', '$password', '$cpassword', '$gender', '$hobbies', '$city', '$pname')";
-
-                if (mysqli_query($conn, $query))
-                {
-                    if (move_uploaded_file($tloc,"Images/".$pname))
-                    {
-                        $_SESSION['result'] = "Form Successfully Submitted";
-                        $_SESSION["showing"] = $pname;
-
-                        header("Location: preview.php");
-                        exit();
-                    }
-                    else
-                    {
-                        echo "Nooo";
-                    }
-                }
-                header("Location: PHPForm.php");
-                exit();
-            }
-            else
-            {
-                header("Location: PHPForm.php");
-                exit();
-            }
-        }
-        else
-        {
-            header("Location: PHPForm.php");
+            $_SESSION["dbErr"] = "Database error: " . mysqli_error($conn);
+            header("Location: index.php");
             exit();
         }
-
-       
+    } else {
+        // Validation failed
+        header("Location: index.php");
+        exit();
     }
+}
 ?>
-
