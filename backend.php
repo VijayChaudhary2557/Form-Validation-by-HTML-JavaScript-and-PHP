44fca67936ca619cf90f93d4d9df29ea01207fb9
VijayChaudhary2557 <155866301+VijayChaudhary2557@users.noreply.github.com>
2025-05-17 21:18:19 +0530

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    $conn = mysqli_connect("localhost","root","","php");
+    $conn = mysqli_connect("sql12.freesqldatabase.com","sql12779399","kA4qNbKN3B","sql12779399");
 
     if (isset($_POST["submit"])) {
         $_SESSION["name"] = $_POST["name"];
